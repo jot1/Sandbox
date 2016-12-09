b42bc6511048c05758426c0b9deb5c17f459d4b1
jc450453 <jothundal306@gmailcom>
2016-12-09 20:00:47 +1000

    commit the file

--- oddName.py
+++ oddName.py
@@ -1,3 +1,14 @@
 __author__ = 'jc450453'
 """
 """
+valid=False
+while not valid:
+    name=input("Enter your name:")
+    if name != "" and not name.isspace():
+        valid = True
+        print(valid)
+
+L=len(name)
+for i in range(0,L,2):
+    print(name[i])
+
